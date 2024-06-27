4f3fdac03c0bd35bce2f4bfda50251177700f8ec
RAMIAHMED <ramiahmet68@gmail.com>
2024-06-27 10:52:12 -0700

    sfsd

--- src/router/Subjects/English/Admin/create.js
+++ src/router/Subjects/English/Admin/create.js
@@ -6,7 +6,7 @@ const _ = require("lodash");
 const ApiErrors = require("../../../../utils/validation_error/ApiErrors");
 
 // english model
-const English = require("../../../../models/Subjects_Banks/english/english");
+const English = require("../../../../models/Subjects_Banks/English/english");
 
 // admin model
 const Admin = require("../../../../models/Admin/admin");
--- src/router/Subjects/English/Teacher/create.js
+++ src/router/Subjects/English/Teacher/create.js
@@ -6,7 +6,7 @@ const _ = require("lodash");
 const ApiErrors = require("../../../../utils/validation_error/ApiErrors");
 
 // english model
-const English = require("../../../../models/Subjects_Banks/english/english");
+const English = require("../../../../models/Subjects_Banks/English/english");
 
 // validate body data method
 const Validate_create_question = require("../../../../middleware/joi_validation/Subjects/Teacher/Joi_validate_create_question");
