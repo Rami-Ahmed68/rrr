1704b2ec596150eba06991a0a2a80386819b573f
ramiahmed68 <108447976+Rami-Ahmed68@users.noreply.github.com>
2024-07-23 12:31:10 -0700

    Update get_one.js

--- src/router/Student/get_one.js
+++ src/router/Student/get_one.js
@@ -61,7 +61,7 @@ router.get("/" , async (req , res , next) => {
         const result = {
             "message" : "Student geted successfully",
             // { user data } ot save all users data in one profile objectin vue.js's store
-            "user_data" : _.pick(student , ["_id" , "name" , "avatar" , "email" , "about_me" , "phone_number" , "gender" , "finished_exams" , "points" , "total_gpa" , "List_of_modifiers" , "classes" , "plans" , "class_level" , "joind_at" , "created_by"])
+            "user_data" : _.pick(student , ["_id" , "name" , "avatar" , "email" , "about_me" , "phone_number" , "gender" , "finished_exams" , "points" , "total_gpa" , "List_of_modifiers" , "classes" , "my_plans" , "class_level" , "joind_at" , "created_by"])
         };
 
         // send the result
