3bd71893b431e83e69b64031c21964b76170f465
ramiahmed68 <108447976+Rami-Ahmed68@users.noreply.github.com>
2024-07-29 06:30:54 -0700

    Update Joi_validate_update.js

--- src/middleware/joi_validation/Admin/Joi_validate_update.js
+++ src/middleware/joi_validation/Admin/Joi_validate_update.js
@@ -7,7 +7,7 @@ const Validate_admin_update = (data, next) => {
     name: Joi.string().min(3).max(100),
     password: Joi.string().min(8).max(100),
     gender: Joi.string(),
-    phone_number : Join.string(),
+    phone_number : Joi.string(),
     delete_avatar: Joi.string(), // true to delete the avatar and set the default avatar
   });
 
