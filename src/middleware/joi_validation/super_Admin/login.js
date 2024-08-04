911c11080b2e66831fcc7487d243f12d2d8804f0
ramiahmed68 <108447976+Rami-Ahmed68@users.noreply.github.com>
2024-08-04 05:26:08 -0700

    Update login.js

--- src/middleware/joi_validation/super_Admin/login.js
+++ src/middleware/joi_validation/super_Admin/login.js
@@ -3,8 +3,8 @@ const Joi = require("joi");
 const Validate_super_admin_login = (data) => {
   // create Schema to validate body data
   const Schema = Joi.object().keys({
-    super_admin_email: Joi.string().required().min(5).max(50).email(),
-    super_admin_password: Joi.string().required().min(8).max(100),
+    email: Joi.string().required().min(5).max(50).email(),
+    password: Joi.string().required().min(8).max(100),
   });
 
   // validate body data
