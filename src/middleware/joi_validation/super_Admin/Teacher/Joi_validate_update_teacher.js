e3885c13a502cff4482b4abdeb2224d610a7a372
ramiahmed68 <108447976+Rami-Ahmed68@users.noreply.github.com>
2024-08-09 07:13:19 -0700

    Update Joi_validate_update_teacher.js

--- src/middleware/joi_validation/super_Admin/Teacher/Joi_validate_update_teacher.js
+++ src/middleware/joi_validation/super_Admin/Teacher/Joi_validate_update_teacher.js
@@ -8,13 +8,12 @@ const Validate_teacher_update = (data) => {
     name: Joi.string().min(3).max(100),
     editor: Joi.boolean(),
     subject: Joi.string(),
-    email: Joi.string().min(5).max(50).email(),
     password: Joi.string().min(8).max(100),
     about_me: Joi.string().min(10).max(500),
     gender: Joi.string(),
     delete_avatar: Joi.boolean(),
     class_level: Joi.string(),
-    phone_number: Joi.number().min(10).max(10),
+    phone_number: Joi.string().min(10).max(10),
   });
 
   // validate body data using Schema
