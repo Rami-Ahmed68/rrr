a57987f07a7277f9c031fbf6dd510aea6215e3ac
ramiahmed68 <ramiahmet68@gmail.com>
2024-08-27 19:57:35 +0300

    test

--- src/router/HomeWork/Supper_Admin/update.js
+++ src/router/HomeWork/Supper_Admin/update.js
@@ -33,7 +33,7 @@ const VerifyToken = require("../../../utils/token_methods/VerifyToken");
 const upload_home_work_images = require("../../../utils/multer/upload_hw_images/uploadeMulter");
 
 router.put("/", upload_home_work_images, async (req, res, next) => {
-  try {
+  // try {
 
     // validate body data
     const Error = Validate_hw_update(req.body);
@@ -264,18 +264,18 @@ router.put("/", upload_home_work_images, async (req, res, next) => {
 
     // send the result
     res.status(200).send(result);
-  } catch (error) {
-    // return error
-    return next(
-      new ApiErrors(
-        JSON.stringify({
-          english: `${error} ...`,
-          arabic: "... عذرا خطأ عام",
-        }),
-        500
-      )
-    );
-  }
+  // } catch (error) {
+  //   // return error
+  //   return next(
+  //     new ApiErrors(
+  //       JSON.stringify({
+  //         english: `${error} ...`,
+  //         arabic: "... عذرا خطأ عام",
+  //       }),
+  //       500
+  //     )
+  //   );
+  // }
 });
 
 module.exports = router;
