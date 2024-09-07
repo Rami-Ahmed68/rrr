e5d11d2c6eb54e92a3c0f68e3282719e1d5355d9
ramiahmed68 <ramiahmet68@gmail.com>
2024-09-07 13:28:04 +0300

    fixed the admin_id to super_admin_id in update & delete food's super admin router

--- src/middleware/joi_validation/Food/Admin/Joi_validate_delete_food.js
+++ src/middleware/joi_validation/Food/Admin/Joi_validate_delete_food.js
@@ -3,7 +3,7 @@ const Joi = require("joi");
 const Validate_delete_food = (data) => {
   // create Schema to validate body data using it
   const Schema = Joi.object().keys({
-    admin_id: Joi.string().required(),
+    super_admin_id: Joi.string().required(),
     food_id: Joi.string().required(),
   });
 
