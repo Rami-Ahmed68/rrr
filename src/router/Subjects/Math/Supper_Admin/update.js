6eace2aa151e9c1ff7df65831721e95a10659e47
ramiahmed68 <ramiahmet68@gmail.com>
2024-09-17 22:34:35 +0300

    test

--- src/router/Subjects/Math/Supper_Admin/update.js
+++ src/router/Subjects/Math/Supper_Admin/update.js
@@ -54,9 +54,11 @@ router.put("/", upload_question_images, async (req, res, next) => {
       );
     }
 
+    console.log(req.body.images_for_delete);
     // create a ImagesForDeleteArray
     let ImagesForDeleteArray = req.body.images_for_delete ? JSON.parse(req.body.images_for_delete) : [];
-
+    console.log(req.body.ImagesForDeleteArray);
+    
     // check if the request has new data
     if (
       !req.body.title &&
