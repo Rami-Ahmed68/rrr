29ae8f10e0ec74db0f7e27d04af52fdf12ec1b2e
ramiahmed68 <ramiahmet68@gmail.com>
2024-09-17 22:41:07 +0300

    test

--- src/router/Subjects/Math/Supper_Admin/update.js
+++ src/router/Subjects/Math/Supper_Admin/update.js
@@ -58,8 +58,8 @@ router.put("/", upload_question_images, async (req, res, next) => {
     console.log(req.body.images_for_delete);
     // create a ImagesForDeleteArray
     let ImagesForDeleteArray = req.body.images_for_delete ? JSON.parse(req.body.images_for_delete) : [];
-    console.log(req.body.ImagesForDeleteArray);
-    console.log(typeof req.body.ImagesForDeleteArray);
+    console.log(ImagesForDeleteArray);
+    console.log(typeof ImagesForDeleteArray);
     
     // check if the request has new data
     if (
