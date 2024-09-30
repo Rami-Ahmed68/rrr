3e2feac45348a1f11a499af61f99169d1ac53723
ramiahmed68 <ramiahmet68@gmail.com>
2024-09-30 12:10:50 +0300

    added the phone number to result in get_one file in parents folder

--- src/router/Parent/get_one.js
+++ src/router/Parent/get_one.js
@@ -61,6 +61,7 @@ router.get("/", async (req, res, next) => {
         "name",
         "avatar",
         "gender",
+        "phone_number",
         "children",
         "created_by",
         "created_at",
