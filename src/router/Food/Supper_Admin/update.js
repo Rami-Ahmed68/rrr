4fc4abb9b234d63eccc383045172835bd04ad6f3
ramiahmed68 <ramiahmet68@gmail.com>
2024-10-15 12:27:52 +0300

    test

--- src/router/Food/Supper_Admin/update.js
+++ src/router/Food/Supper_Admin/update.js
@@ -196,13 +196,15 @@ router.put("/", upload_food_images, async (req, res, next) => {
     // check if the request has images's index to delete
     if (ImagesForDelete && ImagesForDelete.length > 0) {
       // chdck if the food image length is equal one or not
+      for (let i = 0; i < ImagesForDelete.length; i++) { 
         ImagesForDelete.forEach(async (image) => {
           // filtering the images 
-          updateFood.images = food.images.filter((img) => img != image);
-
-          // delete the image 
-          await DeleteCloudinary(image, next);
+          updateFood.images = food.images.filter((img) => img != ImagesForDelete[i]);
         });
+
+        // delete the image 
+        await DeleteCloudinary(ImagesForDelete[i], next);
+      }
     }
 
     // check if the request has a new images to upload
